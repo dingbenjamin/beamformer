81108dd0df1f218791a6fc7fef249448aa8577fb
Benjamin Ding <ben@dingbenjamin.com>
2019-06-03 16:07:26 +1000

    Calculate voltage in receiver script

--- scripts/receive.py
+++ scripts/receive.py
@@ -14,14 +14,15 @@ while True:
         sync2 = uart.read(1)
         if int.from_bytes(sync2, byteorder='little') == 77:
             data = uart.read(16)
-            i1 = int.from_bytes(data[0:2], byteorder='little')
-            q1 = int.from_bytes(data[2:4], byteorder='little')
-            i2 = int.from_bytes(data[4:6], byteorder='little')
-            q2 = int.from_bytes(data[6:8], byteorder='little')
-            i3 = int.from_bytes(data[8:10], byteorder='little')
-            q3 = int.from_bytes(data[10:12], byteorder='little')
-            i4 = int.from_bytes(data[12:14], byteorder='little')
-            q4 = int.from_bytes(data[14:16], byteorder='little')
+            i1 = int.from_bytes(data[0:2], byteorder='little') * 3.3 / 16384
+            q1 = int.from_bytes(data[2:4], byteorder='little') * 3.3 / 16384
+            i2 = int.from_bytes(data[4:6], byteorder='little') * 3.3 / 16384
+            q2 = int.from_bytes(data[6:8], byteorder='little') * 3.3 / 16384
+            i3 = int.from_bytes(data[8:10], byteorder='little') * 3.3 / 16384
+            q3 = int.from_bytes(data[10:12], byteorder='little') * 3.3 / 16384
+            i4 = int.from_bytes(data[12:14], byteorder='little') * 3.3 / 16384
+            q4 = int.from_bytes(data[14:16], byteorder='little') * 3.3 / 16384
+
 
             with open('downsampled.csv', 'a') as f:
                 data_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
